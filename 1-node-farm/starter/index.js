a4307facb9065d521f096c26cd26ecc1ee05f780
Ashish Barthwal <125344587+Ashish13112001@users.noreply.github.com>
2025-12-30 23:07:01 +0530

    create simple server with node

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,8 +1,22 @@
 const fs = require('node:fs');
+const http = require('node:http')
 
+//Read and Write file
 const textIn = fs.readFileSync('./txt/input.txt','utf-8');
 console.log(textIn);
 
 const textOutput = `This is the content of Avacado:  ${textIn} created on ${Date.now()}`;
 fs.writeFileSync('./txt/start.txt', textOutput);
 
+///////////////////////////////////
+
+//Create server
+
+const server = http.createServer((req, res) => {
+    console.log('--- ', req);
+    res.end('Hello from server!');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('listen to requests on port 8000');
+});
