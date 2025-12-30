64027c8fe804256d91345b24174af0c12ccc1803
Ashish Barthwal <125344587+Ashish13112001@users.noreply.github.com>
2025-12-30 23:36:42 +0530

    Routing in node

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,6 @@
 const fs = require('node:fs');
 const http = require('node:http')
+const url = require('node:url')
 
 //Read and Write file
 const textIn = fs.readFileSync('./txt/input.txt','utf-8');
@@ -13,8 +14,22 @@ fs.writeFileSync('./txt/start.txt', textOutput);
 //Create server
 
 const server = http.createServer((req, res) => {
-    console.log('--- ', req);
-    res.end('Hello from server!');
+    // console.log('--- ', req);
+    // console.log('=== ', req.url)
+    const pathName = req.url;
+    
+    if(pathName === '/' || pathName === '/overview'){
+        res.end('This is Overview');
+    }else if(pathName === '/product'){
+        res.end('This is Product');
+    }else{
+        res.writeHead(404, {
+            'content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1> page not found </h1>');
+    }
+    
 });
 
 server.listen(8000, '127.0.0.1', () => {
