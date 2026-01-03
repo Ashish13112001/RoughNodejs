721e2b7bb4edf4ffada7e8e84ddbe79fd6038abc
Ashish Barthwal <125344587+Ashish13112001@users.noreply.github.com>
2026-01-03 17:32:21 +0530

    15-Html Templating

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,44 +1,70 @@
-const fs = require('node:fs');
-const http = require('node:http')
-const url = require('node:url')
-
-//Read and Write file
-const textIn = fs.readFileSync('./txt/input.txt','utf-8');
-console.log(textIn);
-
-const textOutput = `This is the content of Avacado:  ${textIn} created on ${Date.now()}`;
-fs.writeFileSync('./txt/start.txt', textOutput);
-
-///////////////////////////////////
+const fs = require("node:fs");
+const http = require("node:http");
+const url = require("node:url");
 
 // ye top level code hota h jo 1 baar execute hota h jab server start hota h to agar m data synchronously bhi fetch karta hu to koi problem nahi hogi
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 //Create server
+
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
 // ye vo data h jo baar-baar hit hoga jab bhi server s request aaegi
 const server = http.createServer((req, res) => {
-    // console.log('--- ', req);
-    // console.log('=== ', req.url)
-    const pathName = req.url;
-    
-    if(pathName === '/' || pathName === '/overview'){
-        res.end('This is Overview');
-    }else if(pathName === '/product'){
-        res.end('This is Product'); // end -- used to send back a string
-    }else if (pathName === '/api'){
-        res.writeHead(200, {'content-type': 'application/json'});
-        res.end(data);        
-    }else{
-        res.writeHead(404, {
-            'content-type': 'text/html',
-            'my-own-header': 'hello-world'
-        });
-        res.end('<h1> page not found </h1>');
-    }
-    
+  // console.log('--- ', req);
+  // console.log('=== ', req.url)
+  const pathName = req.url;
+
+  //Overview
+  if (pathName === "/" || pathName === "/overview") {
+    res.writeHead(200, { "content-type": "text/html" });
+
+    const cardHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+      console.log('-pp=-', cardHtml)
+    const output = tempOverview.replace(/{%PRODUCT_CARD%}/g, cardHtml);
+    res.end(output);
+  } else if (pathName === "/product") {
+    res.end("This is Product"); // end -- used to send back a string
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1> page not found </h1>");
+  }
 });
 
-server.listen(8000, '127.0.0.1', () => {
-    console.log('listen to requests on port 8000');
+server.listen(8000, "127.0.0.1", () => {
+  console.log("listen to requests on port 8000");
 });
