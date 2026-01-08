2efdab5ccd7b5f02c11954aad94d8e0b329c1d63
Ashish Barthwal <125344587+Ashish13112001@users.noreply.github.com>
2026-01-09 00:34:09 +0530

    50-setting up express and basic routing

---
+++ 4-natours/starter/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello from server side...' });
+});
+app.post('/', (req, res) => {
+    res.status(200).send('Post from server side');
+})
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
