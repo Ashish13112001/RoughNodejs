1c228c7ad63de6a27a1079eaedf2435cffd481a5
Ashish Barthwal <125344587+Ashish13112001@users.noreply.github.com>
2026-01-10 00:07:53 +0530

    52-Handling get request

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,13 +1,21 @@
+const fs = require('node:fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello from server side...' });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
 });
-app.post('/', (req, res) => {
-    res.status(200).send('Post from server side');
-})
 
 const port = 3000;
 app.listen(port, () => {
