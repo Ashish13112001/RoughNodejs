d13b97f651a33b56efa486b85e8bda5208d250d4
Ashish Barthwal <125344587+Ashish13112001@users.noreply.github.com>
2026-01-10 22:11:05 +0530

    64-Params middleware

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -4,7 +4,15 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
-
+exports.checkID = (req, res, next, val) => {
+  if (+req.params.id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  next();
+};
 
 exports.getAllTour = (req, res) => {
   res.status(200).json({
@@ -21,13 +29,6 @@ exports.getTour = (req, res) => {
   // console.log('-=-= ', req.params);
   const id = +req.params.id;
   const tour = tours.find((el) => el.id === id);
-
-  if (!tour) {
-    res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
 
   res.status(200).json({
     status: 'success',
@@ -60,13 +61,6 @@ exports.createTour = (req, res) => {
 
 //we didn't actually update any data (It just demo how patch work)
 exports.updateTour = (req, res) => {
-  if (+req.params.id > tours.length) {
-    res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
   res.status(200).json({
     status: 'success',
     data: {
@@ -77,13 +71,6 @@ exports.updateTour = (req, res) => {
 
 //we didn't actually delete any data (It just demo how delete work)
 exports.deleteTour = (req, res) => {
-  if (+req.params.id > tours.length) {
-    res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
   res.status(204).json({
     status: 'success',
     data: null,
--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -5,6 +5,9 @@ const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
+//This middleware (param middleware) only run when "/api/v1/tours" hit with id => "/api/v1/tours/4"
+router.param('id', tourController.checkID);
+
 router
   .route('/')
   .get(tourController.getAllTour)
