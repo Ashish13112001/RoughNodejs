e8fd0ae47fe79dd5dba3103ef7214effbaef96dc
Ashish Barthwal <125344587+Ashish13112001@users.noreply.github.com>
2026-01-11 11:06:50 +0530

    67-Environment variables

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,6 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' }); //app file ko require karne se pehele environment variables ko set karlo
+
 const app = require('./app');
 
-const port = 3000;
+console.log('=====> ', app.get('env')); //Ye express batata h konse environment m h
+// console.log('---------> ', process.env); //Ye Node batata  h konse environment m h
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
