f8aeb7f1eac4e793d7e896575c2995be3f653385
Ashish Barthwal <125344587+Ashish13112001@users.noreply.github.com>
2026-01-13 23:48:04 +0530

    83-connecting database with express app (with mongoose)

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' }); //app file ko require karne se pehele environment variables ko set karlo
 
@@ -5,6 +6,12 @@ const app = require('./app');
 
 console.log('=====> ', app.get('env')); //Ye express batata h konse environment m h
 // console.log('---------> ', process.env); //Ye Node batata  h konse environment m h
+
+const DB = process.env.DATABASE_LOCAL;
+
+mongoose.connect(DB).then((con) => {
+  console.log('DB connection successful!');
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
