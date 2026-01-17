16c80bf44e32ac5abce275fc79b5e1105c50f282
Ashish Barthwal <125344587+Ashish13112001@users.noreply.github.com>
2026-01-17 22:17:58 +0530

    95-Filtering

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -2,7 +2,27 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTour = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    // console.log('-- --- ', req.query, queryObj);
+
+    const query = Tour.find(queryObj); // This find method return a query (ask to chatgpt and learn more)
+
+    /* This is alternate way for query
+    const query = Tour.find()
+      .where('duration')
+      .equals(5)
+      .where('difficulty')
+      .equals('easy');
+    */
+
+    //EXECUTE QUERY
+    const tours = await query;
+
+    //SEND RESPONSE
     res.status(200).json({
       status: 'success',
       result: tours.length,
