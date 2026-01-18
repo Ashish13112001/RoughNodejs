7fb49416cd3602a2895f4a84de936a80dca00fb8
Ashish Barthwal <barthwal13nov@gmail.com>
2026-01-18 16:14:01 +0530

    105-Document middleware

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -64,8 +64,8 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      // message: err,
-      message: 'Invalid data sent!',
+      message: err,
+      // message: 'Invalid data sent!',
     });
   }
 };
--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have duration'],
@@ -64,6 +66,17 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeka').get(function () {
   return this.duration / 7;
 });
+
+//4 types of middleware in mongodb -- document, query, aggregate, model
+
+//DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre('save', function () {
+  // console.log(this);
+  this.slug = slugify(this.name, { lower: true });
+});
+// tourSchema.post('save', function (doc) {
+//   console.log('😂', doc);
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
