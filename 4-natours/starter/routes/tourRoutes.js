857ce49bcda88a7af93a4d91111f1344b95518df
Ashish Barthwal <barthwal13nov@gmail.com>
2026-01-18 11:54:17 +0530

    103-Aggrigation pipeline -- Unwinding and Projecting

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -10,6 +10,7 @@ router
   .get(tourController.aliasTopTour, tourController.getAllTour);
 
 router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router
   .route('/')
