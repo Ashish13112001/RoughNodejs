deb6ace758885d9d2ce1ffeb9e4c9a49c76d6848
Ashish Barthwal <barthwal13nov@gmail.com>
2026-02-13 07:05:55 +0530

    122, 123

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+process.on('uncaughtException', err => {
+  console.log("UNCAUGHT EXCEPTION... Shutting down!");
+  console.log(err.name, err.message);
+  process.exit(1);
+}); 
+
 dotenv.config({ path: './config.env' }); //app file ko require karne se pehele environment variables ko set karlo
 
 const app = require('./app');
@@ -14,6 +21,16 @@ mongoose.connect(DB).then((con) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log("Unhandled Rejection... Shutting down!");
+  server.close(() => { //ye server ko time det h jo bhi pending req h complete karo, kyuki process.exit() time nahi deta-> ye ekdum band kar deta h(exit kar deta h)
+    process.exit(1); //0 for success and 1 for uncaught exception
+  })
+});
+console.log(x);
