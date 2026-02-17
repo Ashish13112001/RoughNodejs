8f090361d802b090a95d92fe6bba01315ac47355
Ashish Barthwal <barthwal13nov@gmail.com>
2026-02-17 06:50:56 +0530

    127-Managing password

--- 4-natours/starter/models/userModel.js
+++ 4-natours/starter/models/userModel.js
@@ -1,30 +1,48 @@
 const mongoose = require('mongoose');
 const validator = require('validator'); //It is 3rd party library user for validation
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "Please tell us your name"]
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true, //It transform the email in lowercase
+    validate: [validator.isEmail, 'Please provide a valid email'], //It is 3rd party library user for validation
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please provide a Confirm password'],
+    validate: {
+      //This only work on Create and SAVE!!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'password are not same!',
     },
-    email: {
-        type: String,
-        required: [true, "Please provide your email"],
-        unique: true,
-        lowercase: true, //It transform the email in lowercase
-        validator: [validator.isEmail, 'Please provide a valid email'] //It is 3rd party library user for validation
-    },
-    photo: String,
-    password: {
-        type: String,
-        required: [true, "Please provide a password"],
-        minlength: 8
-    },
-    passwordConfirm: {
-        type: String,
-        required: [true, "Please provide a Confirm password"],
-    },
-})
+  },
+});
 
+userSchema.pre('save', async function (next) {
+  //Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  //Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+  //Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  // next(); // I think jab async/await use hota h to next nahi lagate(do chatgpt)
+});
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
